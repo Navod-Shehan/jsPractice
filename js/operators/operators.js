8a5ce4eacae87f304545aa372e6d7e3a85a21507
Navod-Shehan <2019cs196@stu.ucsc.lk>
2023-05-20 13:34:13 +0530

    switch case and many more

--- js/operators/operators.js
+++ js/operators/operators.js
@@ -113,3 +113,83 @@ console.log(age);
 // Exponentiation with equal
 age **= 2;
 console.log(age);
+
+// --------------------------------
+// -----Comparison Operators-------
+// --------------------------------
+
+var a = 20;
+var b = '20';
+var c = 5;
+var d = 15;
+
+// Equal to
+if(a == b){
+    // Equal value and equal type
+    if(a === b){
+        console.log('Both value and type of a is equal to b');
+    }
+    // Not equal value or not equal type
+    else if(a !== b){
+        console.log('Only value or type of a is equal to b');
+    }
+}
+// Greater than or equal to
+else if(a >= 10){
+    // Greater than
+    if(a > 10){
+        console.log('a is greater than 10');
+    }
+    // Equal to
+    else if(a == 10){
+        console.log('a is equal to 10');
+    }
+}
+// Not equal
+else if(a != c){
+    // Less than
+    if(a < c){
+        console.log('a is less than c');
+    }
+    else{
+        console.log('a is greater than c');
+    }
+}
+else{
+    // Less than or equal to
+    if(c <= d){
+        // if(c <= 5){
+        //     console.log('c is less than or equal to 5');
+        // }
+        // else{
+        //     console.log('c is greater than to 5');
+        // }
+        
+        // Ternary operator
+        c <= 5 ? console.log('c is less than or equal to 5') : console.log('c is greater than to 5');
+    }
+    else{
+        console.log('c is greater d');
+    }
+}
+
+
+var e = 20;
+var f = 10;
+var g = 5;
+
+// And
+if(e == 10 && e == f){
+    console.log('It is good');
+}
+// Or
+else if(e > 10 || g > f){
+    console.log('It is ok');
+}
+// Not
+else if(e != g){
+    console.log('It is normal');
+}
+else{
+    console.log("It is bad");
+}
